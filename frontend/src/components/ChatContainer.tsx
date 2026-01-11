c0d87cea3e75deb662a91f4ea5111de6ca14f33e
Howard Sun <howardsun@pm.me>
2026-01-10 22:46:27 -0500

    remove unused imports

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function ChatContainer() {
   return <div>ChatContainer</div>;
 }
--- frontend/src/components/ContactList.tsx
+++ frontend/src/components/ContactList.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function ContactList() {
   return <div>ContactList</div>;
 }
